01247df7eddd609b5a35c239c62da6a431282fa0
JoseNavarroMiranda <nqvarr01234@gmail.com>
2026-08-01 14:02:18 -0600

    integracion final con railway

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-833106ff'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "/index.html",
-    "revision": "0.m14nl3um08c"
+    "revision": "0.cr8ch2daq8s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
